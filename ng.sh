4a0791ede0c12f19f992be795aa845b73303ad77
M3tatr0N_ <109506437+0x-raafet@users.noreply.github.com>
2024-01-05 14:19:19 +0100

    Create ng.sh

---
+++ ng.sh
@@ -0,0 +1,6 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "Download ngrok"
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP 
